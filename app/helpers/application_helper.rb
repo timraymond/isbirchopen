051f58285dce2e007af64b7e9e57ccd7b4c1537f
Tim Raymond <xtjraymondx@gmail.com>
2012-03-04 22:12:23 -0500

    Completed Styling and Integration
    Completed styling of the page and integration of the new design
    into the application. Status lights can be found over on Pixelapse.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def status_light_image(location)
+    if location.status
+      image_tag "location-open.png"
+    else
+      image_tag "location-closed.png"
+    end
+  end
 end
