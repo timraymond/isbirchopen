673cb0d1ac9b00d524aff26fccacb32956b770a8
Tim Raymond <xtjraymondx@gmail.com>
2012-03-05 20:29:53 -0500

    Changed Index of Page and Removed Default Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Diningstatus::Application.routes.draw do
 
-  resources :locations
+
+  root :to => 'locations#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
