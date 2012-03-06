788d3efcd8de45a5ff939d7c75185cee9a47b315
Tim Raymond <xtjraymondx@gmail.com>
2012-03-05 20:32:30 -0500

    Added Seed Data
    Most of the Locations that people care about at UMD were added
    along with their complete set of hours.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,76 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Location.delete_all
+Hour.delete_all
+
+# Res
+hours = []
+hours << Hour.create(open_hour: 7, open_minute: 0, closed_hour: 19, closed_minute: 30, day: "monday")
+hours << Hour.create(open_hour: 7, open_minute: 0, closed_hour: 19, closed_minute: 30, day: "tuesday")
+hours << Hour.create(open_hour: 7, open_minute: 0, closed_hour: 19, closed_minute: 30, day: "wednesday")
+hours << Hour.create(open_hour: 7, open_minute: 0, closed_hour: 19, closed_minute: 30, day: "thursday")
+hours << Hour.create(open_hour: 7, open_minute: 0, closed_hour: 19, closed_minute: 30, day: "friday")
+
+### Weekend Hours
+hours << Hour.create(open_hour: 9, open_minute: 0, closed_hour: 14, closed_minute: 0, day: "saturday")
+hours << Hour.create(open_hour: 17, open_minute: 0, closed_hour: 19, closed_minute: 0, day: "saturday")
+
+hours << Hour.create(open_hour: 9, open_minute: 0, closed_hour: 14, closed_minute: 0, day: "sunday")
+hours << Hour.create(open_hour: 17, open_minute: 0, closed_hour: 19, closed_minute: 0, day: "sunday")
+Location.create(name: "Res", hours: hours)
+
+# Birch Grill
+hours = []
+hours << Hour.create(open_hour: 11, open_minute: 30, closed_hour: 24, closed_minute: 0, day: "monday")
+hours << Hour.create(open_hour: 11, open_minute: 30, closed_hour: 24, closed_minute: 0, day: "tuesday")
+hours << Hour.create(open_hour: 11, open_minute: 30, closed_hour: 24, closed_minute: 0, day: "wednesday")
+hours << Hour.create(open_hour: 11, open_minute: 30, closed_hour: 27, closed_minute: 0, day: "thursday")
+hours << Hour.create(open_hour: 11, open_minute: 30, closed_hour: 27, closed_minute: 0, day: "friday")
+hours << Hour.create(open_hour: 18, open_minute: 0, closed_hour: 25, closed_minute: 0, day: "saturday")
+hours << Hour.create(open_hour: 17, open_minute: 0, closed_hour: 24, closed_minute: 0, day: "sunday")
+Location.create(name: "Birch", hours: hours)
+
+# Maple Ridge
+hours = []
+hours << Hour.create(open_hour: 19, open_minute: 0, closed_hour: 24, closed_minute: 0, day: "monday")
+hours << Hour.create(open_hour: 19, open_minute: 0, closed_hour: 24, closed_minute: 0, day: "tuesday")
+hours << Hour.create(open_hour: 19, open_minute: 0, closed_hour: 24, closed_minute: 0, day: "wednesday")
+hours << Hour.create(open_hour: 19, open_minute: 0, closed_hour: 24, closed_minute: 0, day: "thursday")
+hours << Hour.create(open_hour: 19, open_minute: 0, closed_hour: 24, closed_minute: 0, day: "sunday")
+Location.create(name: "Maple Ridge", hours: hours)
+
+# Scally Wagon
+hours = []
+hours << Hour.create(open_hour: 21, open_minute: 0, closed_hour: 26, closed_minute: 0, day: "thursday")
+hours << Hour.create(open_hour: 21, open_minute: 0, closed_hour: 26, closed_minute: 0, day: "friday")
+hours << Hour.create(open_hour: 21, open_minute: 0, closed_hour: 26, closed_minute: 0, day: "saturday")
+hours << Hour.create(open_hour: 21, open_minute: 0, closed_hour: 24, closed_minute: 0, day: "sunday")
+Location.create(name: "Scally Wagon", hours: hours)
+
+# Grill Nation
+hours = []
+hours << Hour.create(open_hour: 11, open_minute: 0, closed_hour: 20, closed_minute: 30, day: "monday")
+hours << Hour.create(open_hour: 11, open_minute: 0, closed_hour: 20, closed_minute: 30, day: "tuesday")
+hours << Hour.create(open_hour: 11, open_minute: 0, closed_hour: 20, closed_minute: 30, day: "wednesday")
+hours << Hour.create(open_hour: 11, open_minute: 0, closed_hour: 20, closed_minute: 30, day: "thursday")
+hours << Hour.create(open_hour: 11, open_minute: 0, closed_hour: 19, closed_minute: 30, day: "friday")
+Location.create(name: "Grill Nation", hours:hours)
+
+# Oak Glen
+hours = []
+hours << Hour.create(open_hour: 17, open_minute: 0, closed_hour: 24, closed_minute: 0, day: "monday")
+hours << Hour.create(open_hour: 17, open_minute: 0, closed_hour: 24, closed_minute: 0, day: "tuesday")
+hours << Hour.create(open_hour: 17, open_minute: 0, closed_hour: 24, closed_minute: 0, day: "wednesday")
+hours << Hour.create(open_hour: 17, open_minute: 0, closed_hour: 24, closed_minute: 0, day: "thursday")
+Location.create(name: "Oak Glen", hours: hours)
+
+# Corsair Cafe
+hours = []
+hours << Hour.create(open_hour: 7, open_minute: 0, closed_hour: 20, closed_minute: 30, day: "monday")
+hours << Hour.create(open_hour: 7, open_minute: 0, closed_hour: 20, closed_minute: 30, day: "tuesday")
+hours << Hour.create(open_hour: 7, open_minute: 0, closed_hour: 20, closed_minute: 30, day: "wednesday")
+hours << Hour.create(open_hour: 7, open_minute: 0, closed_hour: 20, closed_minute: 30, day: "thursday")
+hours << Hour.create(open_hour: 7, open_minute: 0, closed_hour: 17, closed_minute: 0, day: "thursday")
+Location.create(name: "Corsair Cafe", hours: hours)
