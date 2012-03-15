09cd03866f9e1129c54935b6f1daf498215b88cc
Tim Raymond <xtjraymondx@gmail.com>
2012-03-15 00:59:27 -0400

    Rollback Caching
    Caching causes false opening bugs currently. Rolling
    back the feature.

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,16 +5,16 @@ class Location < ActiveRecord::Base
   def status(time = Time.now)
     #returns true if the location is open, false if not
 
-    if $redis.exists self.name
-      closing = Time.parse($redis.get self.name)
+    #if $redis.exists self.name
+    #  closing = Time.parse($redis.get self.name)
 
-      if time < closing
-        return true
-      else
+    #  if time < closing
+    #    return true
+    #  else
         # Closing is stale
-        $redis.del self.name
-      end
-    end
+    #    $redis.del self.name
+    #  end
+    #end
 
       # Hit the database if cache is stale or the location was previously closed
     closing = self.hours.reduce(nil) do |found, hour|
@@ -23,7 +23,7 @@ class Location < ActiveRecord::Base
       end
     end
 
-    $redis.set(self.name, closing) if closing #set the cache to the new closing time
+    #$redis.set(self.name, closing) if closing #set the cache to the new closing time
     closing
   end
 end
